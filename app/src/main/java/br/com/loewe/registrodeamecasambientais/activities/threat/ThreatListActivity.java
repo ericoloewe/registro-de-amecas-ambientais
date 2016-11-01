829a863d6f9d43cd8ae67d0a9a6b5a6e3cd870d2
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-10-31 22:57:09 -0200

    fix(threat): adjust list.

--- app/src/main/java/br/com/loewe/registrodeamecasambientais/activities/threat/ThreatListActivity.java
+++ app/src/main/java/br/com/loewe/registrodeamecasambientais/activities/threat/ThreatListActivity.java
@@ -1,27 +1,31 @@
 package br.com.loewe.registrodeamecasambientais.activities.threat;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import br.com.loewe.registrodeamecasambientais.R;
 
-public class ThreatListActivity extends ActionBarActivity {
+public class ThreatListActivity extends AppCompatActivity {
 
     private Button btnUpdate;
+    private ListView threatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_threat_list);
+        setContentView(R.layout.activity_list_threat);
 
         loadViewElements();
         bindUiEvents();
     }
 
     private void loadViewElements() {
+        threatList = (ListView) findViewById(R.id.threatList);
         btnUpdate = (Button) findViewById(R.id.btnUpdate);
     }
 
